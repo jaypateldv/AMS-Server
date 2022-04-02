f7f51ecd9fc50e4af9da211e9336cc87fa0b72b8
jaypatel-simform <jaypatel@simformsolutions.com>
2022-04-02 15:01:07 +0530

    created login and signup routes  with token based authentication

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const router = new express.Router()
-
+const  userControllers = require("../controllers/user.controllers")
 router.get("/",async(req,res,)=>{
     res.send("from user")
 })
 
+
+router.post('/signup',userControllers.signUp)
+router.post('/login',userControllers.login)
 module.exports= router
 
 
