090279fd25a99c3c1cd5880681913918e4f328ba
Ansh_shah_16 <ansh.s@simformsolutions.com>
2022-04-03 10:10:06 +0530

    Add comments in User & Organizer route

--- src/routes/organizerRoute.js
+++ src/routes/organizerRoute.js
@@ -8,12 +8,25 @@ router.get("/", async (req, res,) => {
     res.send("from organizer")
 })
 
+// Display all Auditorium to Event Organizer
 router.get("/auditorium", [authToken, isOrganizer], organizerCtrl.findAuditorium)
+
+// Display AvailableSlots
 router.get("/getAvailableSlots", [authToken, isOrganizer], organizerCtrl.getalltimeslots)
+
+// Book Auditorium for an Event
 router.post("/bookAuditorium", [authToken, isOrganizer], organizerCtrl.bookAuditorium)
+
+// Display all Events
 router.get("/allEvents", [authToken, isOrganizer], organizerCtrl.allEvents)
+
+// Display Purchase Histories for specific organizer
 router.get("/purchaseHistory", [authToken, isOrganizer], organizerCtrl.purchaseHistory)
+
+// Update Event details By Id 
 router.patch('/update/eventDetails/:eventId', [authToken, isOrganizer], organizerCtrl.updateEventById)
+
+// Check Payment Status of Organizer
 router.post("/audiBookingPayment/:status", [authToken, isOrganizer], organizerCtrl.auditoriumBookingPayment)
 
 module.exports = router
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,17 +1,27 @@
 const express = require("express")
 const router = new express.Router()
 const userControllers = require("../controllers/user.controllers")
-const {isManagerSignUp} = require('../middlewares/roleMiddleware')
+const { isManagerSignUp } = require('../middlewares/roleMiddleware')
 const authToken = require('../middlewares/authMiddleware')
 router.get("/", async (req, res,) => {
     res.send("from user")
 })
 
-router.post('/signup',isManagerSignUp, userControllers.signUp)
+// Handling user Sign-up
+router.post('/signup', isManagerSignUp, userControllers.signUp)
+
+// Handling user Log-in
 router.post('/login', userControllers.login)
-router.get('/profile',authToken,userControllers.profile)
-router.patch('/profileupdate',authToken,userControllers.profileUpdate)
-router.patch('/deleteaccount',authToken,userControllers.deleteAccount)
+
+// Display User Profile
+router.get('/profile', authToken, userControllers.profile)
+
+// Update User Profile
+router.patch('/profileupdate', authToken, userControllers.profileUpdate)
+
+// Delete User
+router.patch('/deleteaccount', authToken, userControllers.deleteAccount)
+
 module.exports = router
 
- 
+
