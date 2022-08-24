f861fa0b92490c53e47c88f73cb941c2074ea85e
Jay Patel <jaypatel@simformsolutions.com>
2022-08-24 12:22:54 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const {app} = require("./routes/index")
-const PORT = process.env.PORT||3000
-app.listen(PORT , ()=>{
+const { app } = require("./routes/index")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log(`server is listing on PORT : ${PORT}`)
 })
